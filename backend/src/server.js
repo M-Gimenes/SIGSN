19e1450c15e54604338889cd0cf87b09241ab0c4
M-Gimenes <contato.matheusgimenes@gmail.com>
2026-04-09 16:01:56 -0300

    swagger update

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,25 @@
 import express from 'express';
 import cors from 'cors';
+import { existsSync, readFileSync } from 'fs';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import swaggerUi from 'swagger-ui-express';
 import routes from './routes.js';
 import './config/database-connection.js';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+const swaggerPath = join(__dirname, '..', 'swagger.json');
+if (existsSync(swaggerPath)) {
+  const swaggerDocument = JSON.parse(readFileSync(swaggerPath, 'utf-8'));
+  app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+}
+
 app.use(routes);
 
 app.use((err, _req, res, _next) => {
